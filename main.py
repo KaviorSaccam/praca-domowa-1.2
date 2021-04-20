0b711f1c0868bae4c4b23a27194ae0b66bd7fbcb
KaviorSaccam <82527687+KaviorSaccam@users.noreply.github.com>
2021-04-20 20:22:12 +0200

    P_D_1.2

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/method/{name}")
+def method_name_view(name: str):
+	return f"{name}"
