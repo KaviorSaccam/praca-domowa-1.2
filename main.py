c9b0a9c36ea456353818fe899312deb03baf70da
KaviorSaccam <82527687+KaviorSaccam@users.noreply.github.com>
2021-04-20 21:30:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,24 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {'message': 'Hello world!'}
+    return {"message": "Hello world!"}
 
-@app.get("/method/{name}")
-def method_name_view(name: str):
-	return {"method":name}
+@app.get("/method")
+def get_method():
+	return {"method":"GET"}
+
+@app.put("/method")
+def put_method():
+	return {"method":"PUT"}
+
+@app.options("/method")
+def options_method():
+	return {"method":"OPTIONS"}
+
+@app.delete("/method")
+def delete_method():
+	return {"method":"DELETE"}
+
+@app.post("/method")
+def post_method():
+	return {"method":"POST"}
